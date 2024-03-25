2d62e48042a5fea705f1efae171d9d92521a3673
Jean Yves Ehouman <yvesehouman09@gmail.com>
2024-03-25 17:04:59 +0900

    Create new components

--- src/components/marketing/pageSection/Header.js
+++ src/components/marketing/pageSection/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Logo from "../../../medias/svg/logo-burger-house.svg";
 import BagIcon from "../../../medias/svg/ico-bag-clickAndCollect.svg";
+import Button from '../../elements/Button';
 
 export default function Header() {
   return (
@@ -9,9 +10,16 @@ export default function Header() {
             <img src={Logo} alt='logo' className='w-64' />
         </div>
 
-        <div className='w-full text-secondary flex items-center justify-end'>
-            <img src={BagIcon} alt='Bag Icon' className='w-5 h-5 mr-1' />
-            <span>Commandez votre repas en ligne</span>
+        <div className='w-full text-secondary'>
+            <div className='flex items-center justify-end'>
+              <img src={BagIcon} alt='Bag Icon' className='w-5 h-5 mr-1' />
+              <span>Commandez votre repas en ligne</span>
+            </div>
+            <div>
+              
+            <Button></Button>
+
+            </div>
         </div>
         
         
